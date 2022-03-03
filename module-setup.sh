20ddae1ed70697f7ab7a1692dc5892cef3c8254d
Fabian Vogt <fvogt@suse.de>
2022-03-03 13:39:38 +0100

    Import combustion 0.2 from OBS
    Matches openSUSE:Factory/combustion at aa5be0340a98ceea5b9917d7cb404e3f.

---
+++ module-setup.sh
@@ -0,0 +1,19 @@
+depends() {
+	echo bash network systemd url-lib
+}
+
+install() {
+	inst_simple "${moddir}/combustion.service" "${systemdsystemunitdir}/combustion.service"
+	inst_simple "${moddir}/combustion-prepare.service" "${systemdsystemunitdir}/combustion-prepare.service"
+	inst_simple "${moddir}/combustion.rules" "/etc/udev/rules.d/70-combustion.rules"
+	mkdir -p "${initdir}/${systemdsystemunitdir}/initrd.target.requires/"
+	ln_r "../combustion.service" "${systemdsystemunitdir}/initrd.target.requires/combustion.service"
+	inst_multiple awk chroot findmnt grep rmdir
+	inst_simple "${moddir}/combustion" "/usr/bin/combustion"
+
+	# Wait up to 10s (30s on aarch64) for the config drive
+	devtimeout=10
+	[ "$(uname -m)" = "aarch64" ] && devtimeout=30
+	mkdir -p "${initdir}/${systemdsystemunitdir}/dev-combustion-config.device.d/"
+	echo -e "[Unit]\nJobTimeoutSec=${devtimeout}" > "${initdir}/${systemdsystemunitdir}/dev-combustion-config.device.d/timeout.conf"
+}
