10bab362744962a29b621708368dc3b513665d3d
Fabian Vogt <fvogt@suse.de>
2022-07-05 15:29:03 +0200

    Replace invalid use of ln_r in module-setup.sh
    ln_r expects an absolute path as first argument, passing a relative path
    instead behaves as if it was relative to /. That behaviour lead to dangling
    symlinks in the .target.requires directory.
    
    Use the same method as dracut's own modules.

--- module-setup.sh
+++ module-setup.sh
@@ -6,8 +6,7 @@ install() {
 	inst_simple "${moddir}/combustion.service" "${systemdsystemunitdir}/combustion.service"
 	inst_simple "${moddir}/combustion-prepare.service" "${systemdsystemunitdir}/combustion-prepare.service"
 	inst_simple "${moddir}/combustion.rules" "/etc/udev/rules.d/70-combustion.rules"
-	mkdir -p "${initdir}/${systemdsystemunitdir}/initrd.target.requires/"
-	ln_r "../combustion.service" "${systemdsystemunitdir}/initrd.target.requires/combustion.service"
+	$SYSTEMCTL -q --root "$initdir" enable combustion.service
 	inst_multiple awk chroot findmnt grep rmdir
 	inst_simple "${moddir}/combustion" "/usr/bin/combustion"
 
