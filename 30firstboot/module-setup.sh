242898b8ec76687aeb6e8a77b568b70d99a8e712
Fabian Vogt <fvogt@suse.de>
2024-02-27 15:56:50 +0100

    Only enable all existing DASDs if it's actually the first boot (bsc#1219849)
    Specify ipldev in the other cases.

--- 30firstboot/module-setup.sh
+++ 30firstboot/module-setup.sh
@@ -14,6 +14,17 @@ install() {
 	$SYSTEMCTL -q --root "$initdir" enable firstboot-detect.service
 	inst_multiple awk grep mount
 
+	if [ "${DRACUT_ARCH:-$(uname -m)}" = "s390x" ]; then
+		# Special behaviour on s390x: On the first boot, enable all DASDs
+		inst_multiple chzdev
+
+		# On all boots, enable at least the IPL dev.
+		# Once the initrd was rebuilt without this module, it should contain
+		# an appropriate rd.dasd parameter instead to trigger enabling.
+		mkdir -p "${initdir}/etc/modprobe.d"
+		echo "options dasd_mod dasd=ipldev" > "${initdir}/etc/modprobe.d/dasd-ipldev.conf"
+	fi
+
 	# Work around https://github.com/systemd/systemd/pull/28718
 	mkdir -p "${initdir}/${systemdsystemunitdir}/initrd-parse-etc.service.d/"
 	echo -e "[Unit]\nConflicts=emergency.target" > "${initdir}/${systemdsystemunitdir}/initrd-parse-etc.service.d/emergency.conf"
