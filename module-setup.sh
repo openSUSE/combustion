6307d92c13bbaf986ac6e91b867360ebd1805cca
Fabian Vogt <fvogt@suse.de>
2024-02-29 18:08:38 +0100

    Use $dracutsysrootdir

--- module-setup.sh
+++ module-setup.sh
@@ -1,6 +1,6 @@
 check() {
 	# Omit if building for this already configured system
-	if [[ $hostonly ]] && [ -e /etc/machine-id ]; then
+	if [[ $hostonly ]] && [ -e "${dracutsysrootdir}/etc/machine-id" ]; then
 		return 255
 	fi
 	return 0
